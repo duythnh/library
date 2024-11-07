3fa8490262d471f7ad16b949d11eff4d50494f92
duythnh <duytle02@gmail.com>
2024-11-07 15:19:03 +0700

    build an interface

--- main.js
+++ main.js
@@ -1,9 +1,46 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, page, read) {
+  this.title = title;
+  this.author = author;
+  this.page = page;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(title, author, page, read) {
+  let book = new Book(title, author, page, read);
+  myLibrary.push(book);
 }
+
+function displayBooksOnPage() {
+  const books = document.querySelector(".books");
+  books.innerHTML = "";
+
+  myLibrary.forEach((book) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    books.appendChild(card);
+
+    for (let key in book) {
+      console.log(`${key}: ${book[key]}`);
+      const para = document.createElement("p");
+      para.textContent = `${key}: ${book[key]}`;
+      card.appendChild(para);
+    }
+  });
+}
+
+addBookToLibrary(
+  "Fire and Blood",
+  "George R. R. Martin",
+  "736 pages",
+  "not read yet"
+);
+addBookToLibrary(
+  "A Dance with Dragons",
+  "George R. R. Martin",
+  "1,056 pages",
+  "not read yet"
+);
+
+displayBooksOnPage();
